e8c8f8f78ee450feb56d83bdb921d369df7fae77
Jitendra <jitendra.sahu.automation@gmail.com>
2025-02-28 16:38:14 +0530

    Third Commit

--- src/interViewPractice/Interview.java
+++ src/interViewPractice/Interview.java
@@ -1,11 +1,26 @@
 package interViewPractice;
 
+import java.util.Scanner;
+
+import org.testng.annotations.Test;
+
 public class Interview {
 	
-	
+	@Test
 	public void factorial() {
 		
-		System.err.println("Factorial code will take place here");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your Number");
+		int a=sc.nextInt();
+		
+		int fact=1;
+		for(int i=1;i<=a;i++) {
+			
+			fact=fact*i;
+			
+		}
+		
+		System.out.println("Factorial of your number is-"+fact);
 	}
 	
 	
